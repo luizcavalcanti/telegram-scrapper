5d317954bf373fdfa325b3d9341dc3f647407d2b
Eduardo Cuducos <cuducos@users.noreply.github.com>
2021-07-02 12:19:41 -0400

    Cria modelo Mensagem

--- telegram_scrapper/core/models.py
+++ telegram_scrapper/core/models.py
@@ -1,13 +1,23 @@
-# from django.db import models
+from django.db import models
 
-# TODO criar um modelo com essa estrutura:
-#      group_id    TEXT NOT NULL,
-#      sender_id   TEXT NOT NULL,
-#      send_date   DATETIME NOT NULL,
-#      message     TEXT,
-#      video       TEXT,
-#      audio       TEXT,
-#      document    TEXT,
-#      photo       TEXT,
-#      forward     BOOLEAN
-# Talvez um modelo para Grupo (mapear group_id e group_name, por ex)
+
+class Message(models.Model):
+    message_id = models.BigIntegerField("ID da mensagem")
+    group = models.CharField("Grupo", max_length=255)
+    sender = models.CharField("remetente", max_length=255)
+    sent_at = models.DateTimeField("enviado em")
+    message = models.TextField("mensagem")
+    video = models.JSONField("vídeo", default=dict)
+    audio = models.JSONField("áudio", default=dict)
+    document = models.JSONField("documento", default=dict)
+    photo = models.JSONField("imagem", default=dict)
+    forwarded = models.BooleanField("encaminhada", default=False)
+
+    class Meta:
+        verbose_name = "mensagem"
+        verbose_name_plural = "mensagens"
+        ordering = ["-sent_at"]
+        indexes = [
+            models.Index(fields=["sent_at"]),
+            models.Index(fields=["group"]),
+        ]
