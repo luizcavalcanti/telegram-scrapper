cf1e2f349823a1d56a259257fddcad3b02f21360
Luiz Cavalcanti <cavalcanti.luiz@gmail.com>
2021-07-11 00:37:00 -0300

    Ajustes para extração funcionar como comando (download de midia desabilitado)
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- telegram_scrapper/core/management/commands/extract.py
+++ telegram_scrapper/core/management/commands/extract.py
@@ -2,9 +2,12 @@ from tempfile import TemporaryDirectory
 
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
-from telethon import TelegramClient
-
+from django.db.utils import IntegrityError
+from telethon import TelegramClient, sync
+from telethon.tl.types import PeerUser
 from telegram_scrapper.core.models import MEDIAS, Message
+
+import glob
 
 
 MESSAGES_PER_QUERY = 1000
@@ -30,46 +33,56 @@ class Command(BaseCommand):
             default=MESSAGES_PER_QUERY,
         )
 
-    def messages_from(self, group, number):
-        return self.telegram_client.get_messages(group, number)
+    def handle(self, *args, **options):
+        query_size = options["limit"]
+        previous_count = Message.objects.count()
+        for group in settings.TELEGRAM_GROUPS:
+            self.update_group_messages(group, query_size)
+
+        total = Message.objects.count()
+        self.stdout.write(
+            self.style.SUCCESS(f"{total - previous_count} new messages saved!")
+        )
+        self.stdout.write(self.style.SUCCESS(f"{total} messages stored!"))
+
+    def update_group_messages(self, group, query_size):
+        self.stdout.write(f"Fetching {group} messages…")
+        messages = self.telegram_client.get_messages(group, query_size)
+        for message in messages:
+            try:
+                self.save_message(message, group)
+            except IntegrityError as e:
+                pass
+            except Exception as e:  # TODO que erros esperamos aqui?
+                raise CommandError(f"Erro baixando mensagens de {group}: {e}")
 
     def save_message(self, message, group):
         obj = Message(
             message_id=message.id,
             group=group,
-            sender=message.from_id.user_id if message.from_id else "channel",
+            sender=self.get_sender(message),
             sent_at=message.date,
             forwarded=bool(message.forward),
         )
 
-        with TemporaryDirectory() as tmp:
-            for kind in MEDIAS:
-                media = getattr(message, kind)
-                if not media:
-                    continue
+        for kind in MEDIAS:
+            media = getattr(message, kind)
+            if not media:
+                continue
 
-                # TODO Mudar campos no modelo de JSONField para FileField e salvar
-                # path = Path(tmp) / media.id
-                # self.telegram_client.download_media(message=media, file=path)
-                setattr(obj, kind, media.to_json())
+            setattr(obj, kind, media.to_json())
 
-            return obj
-            obj.save()
+        # with TemporaryDirectory() as tmp:
+        #     # TODO Mudar campos no modelo de JSONField para FileField e salvar
+        #     path = f"{tmp}/{media.id}"
+        #     if not glob.glob(f"{path}*"):
+        #         self.stdout.write(f"\tdownloading media {media.id}...")
+        #         self.telegram_client.download_media(message=media, file=path)
+        #     return obj
 
-    def update_group_messages(self, group, number):
-        self.stdout.write(f"Fetching {group} messages…")
-        for message in self.messages_from(group, number):
-            try:
-                self.save_message(message, group)
-            except Exception as e:  # TODO que erros esperamos aqui?
-                raise CommandError(f"Erro baixando mensagens de {group}: {e}")
+        obj.save()
 
-    def handle(self, *args, **options):
-        self.stdout.write(options["limit"])
-        previous_count = Message.objects.count()
-        for group in settings.TELEGRAM_GROUPS:
-            self.update_group_messages(group)
-
-        total = Message.objects.count()
-        self.stdout.write(self.style.SUCCESS(f"{total - previous} new messages saved!"))
-        self.stdout.write(self.style.SUCCESS(f"{total} messages stored!"))
+    def get_sender(self, message):
+        return (
+            message.from_id.user_id if type(message.from_id) is PeerUser else "channel"
+        )
