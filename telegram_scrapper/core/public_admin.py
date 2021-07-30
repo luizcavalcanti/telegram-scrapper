cffca5b7de948f468700bf3e2422be40b3fb099b
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2021-07-29 23:51:12 -0300

    Colunas customizadas de conteúdo de midia no public admin
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- telegram_scrapper/core/public_admin.py
+++ telegram_scrapper/core/public_admin.py
@@ -5,11 +5,36 @@ from .models import Message
 
 
 class MessageModelAdmin(PublicModelAdmin):
-	search_fields = ['message']
-	list_display = ('message_id', 'group', 'sender', 'message', 'sent_at')
-	ordering = ['-sent_at']
-	list_filter = ['group']
+    search_fields = ['message']
+    list_display = (
+        'message_id',
+        'group',
+        'sender',
+        'message',
+        'has_audio',
+        'has_image',
+        'has_video',
+        'sent_at',
+    )
+    ordering = ['-sent_at']
+    list_filter = ['group']
 
+    def has_audio(self, obj):
+        return bool(obj.audio)
+
+    def has_image(self, obj):
+        return bool(obj.photo)
+
+    def has_video(self, obj):
+        return bool(obj.video)
+
+    has_audio.boolean = True
+    has_image.boolean = True
+    has_video.boolean = True
+
+    has_audio.short_description = "audio"
+    has_image.short_description = "imagem"
+    has_video.short_description = "video"
 
 
 public_app = PublicApp("core", models=["Message"])
