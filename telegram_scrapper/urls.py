ab80925ceede670895eb9909aca722e5ffd473d5
Luiz Cavalcanti <cavalcanti.luiz@gmail.com>
2021-07-29 22:41:45 -0300

    Adiciona public-admin em /dashboard
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- telegram_scrapper/urls.py
+++ telegram_scrapper/urls.py
@@ -1,6 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-# TODO utilizar django-public-admin
+from telegram_scrapper.core.public_admin import public_admin
 
-urlpatterns = [path("admin/", admin.site.urls)]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("dashboard/", public_admin.urls),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
