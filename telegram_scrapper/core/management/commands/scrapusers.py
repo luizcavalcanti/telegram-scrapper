1dae7566e71ffb461a37bf9c5b4baae023892e6a
Luiz Carlos Cavalcanti <luiz.c@thoughtworks.com>
2021-09-05 15:47:18 -0300

    Comando para scrapping de usuários por grupos
    Signed-off-by: Luiz Carlos Cavalcanti <luiz.c@thoughtworks.com>

---
+++ telegram_scrapper/core/management/commands/scrapusers.py
@@ -0,0 +1,50 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from telethon import TelegramClient, sync
+from telegram_scrapper.core.models import Group, TelegramUser
+
+
+class Command(BaseCommand):
+    help = "Scrap Telegram users from saved messages"
+
+    @property
+    def telegram_client(self):
+        if not getattr(self, "_telegram_client", None):
+            self._telegram_client = TelegramClient(
+                "session_name", settings.TELEGRAM_API_ID, settings.TELEGRAM_API_HASH
+            ).start()
+
+        return self._telegram_client
+
+    def handle(self, *args, **options):
+        previous_count = TelegramUser.objects.count()
+
+        self._get_from_groups()
+
+        total = TelegramUser.objects.count()
+        self.stdout.write(
+            self.style.SUCCESS(f"{total - previous_count} new users found!")
+        )
+        self.stdout.write(self.style.SUCCESS(f"{total} users stored!"))
+
+    def _get_from_groups(self):
+        groups = Group.objects.filter(active=True)
+        for group in groups:
+            self.stdout.write(f"Retrieving users from {group.id}...")
+            participants = self.telegram_client.get_participants(group.id)
+            [self._save_user(p) for p in participants]
+
+    def _save_user(self, user):
+        obj = TelegramUser(
+            user_id=user.id,
+            username=user.username,
+            first_name=user.first_name,
+            last_name=user.last_name,
+            phone=user.phone,
+            photo=user.photo.to_json() if user.photo else None,
+            fake=bool(user.fake),
+            deleted=bool(user.deleted),
+            verified=bool(user.verified),
+        )
+
+        obj.save()
