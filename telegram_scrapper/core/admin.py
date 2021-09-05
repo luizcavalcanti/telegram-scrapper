634db6895430f23cc8e3241373956eb005b96e2a
Luiz Cavalcanti <cavalcanti.luiz@gmail.com>
2021-09-05 15:46:31 -0300

    Adiciona config de djando admin para TelegramUser
    Signed-off-by: Luiz Carlos Cavalcanti <luiz.c@thoughtworks.com>

--- telegram_scrapper/core/admin.py
+++ telegram_scrapper/core/admin.py
@@ -1,15 +1,37 @@
 from django.contrib import admin
 from django.db.models import Count
 from django.db.models.functions import Lower
-from .models import Message, Group
+from django.utils.safestring import mark_safe
+from .models import Message, Group, TelegramUser
 
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
     search_fields = ['message']
-    list_display = ('message_id', 'group', 'sender', 'message', 'sent_at')
+    list_display = ('message_id', 'group', 'link_sender', 'message', 'sent_at')
     ordering = ['-sent_at']
     list_filter = ['group']
+
+    @mark_safe
+    def link_sender(self, obj):
+        return f"<a href=\"/admin/core/telegramuser/{obj.sender}\" />{obj.sender}</a>"
+
+    link_sender.short_description = "Remetente"
+    link_sender.allow_tags = True
+
+
+@admin.register(TelegramUser)
+class TelegramUserAdmin(admin.ModelAdmin):
+    search_fields = ['user_id', 'username', 'first_name', 'last_name', 'phone']
+    list_display = ('user_id', 'username', 'full_name', 'verified')
+    ordering = ['user_id']
+    list_filter = ['verified']
+
+    def full_name(self, obj):
+        return (
+            f"{obj.first_name if obj.first_name else ''}"
+            f" {obj.last_name if obj.last_name else ''}"
+        )
 
 
 @admin.register(Group)
