ba555817e237089e9b458189461dbcd4ca235bbc
Luiz Cavalcanti <cavalcanti.luiz@gmail.com>
2021-09-22 16:37:13 -0300

    Endpoint com listagem de Top X imagens mais compartilhadas

--- telegram_scrapper/api/views.py
+++ telegram_scrapper/api/views.py
@@ -8,6 +8,7 @@ from telegram_scrapper.core.models import Message
 
 
 _DEFAULT_DAYS = 30
+_DEFAULT_LIMIT = 10
 
 
 def messages_per_day(request):
@@ -20,6 +21,22 @@ def messages_per_day(request):
         .order_by('date')
         .values('date')
         .annotate(**{'total': Count('sent_at')})
+    )
+
+    return JsonResponse(list(data), safe=False)
+
+
+def top_images(request):
+    past_days = int(request.GET.get('days', _DEFAULT_DAYS))
+    limit = int(request.GET.get('limit', _DEFAULT_LIMIT))
+    start_date = datetime.today() - timedelta(days=past_days)
+    end_date = datetime.today()
+    data = (
+        Message.objects.filter(sent_at__range=[start_date.date(), end_date.date()])
+        .annotate(count=Count('photo_url'))
+        .order_by('-count')
+        .values('photo_url')
+        .annotate(**{'total': Count('photo_url')})[:limit]
     )
 
     return JsonResponse(list(data), safe=False)
