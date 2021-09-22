9653e6f84455cb4392b6f5e8b63db495de1945c8
Luiz Carlos Cavalcanti <luiz.c@thoughtworks.com>
2021-09-22 16:37:13 -0300

    Endpoint com listagem de Top X imagens mais compartilhadas

--- telegram_scrapper/api/__init__.py
+++ telegram_scrapper/api/__init__.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import messages_per_day
+from .views import messages_per_day, top_images
 
 urls = (
-    [path('messages_per_day', views.messages_per_day, name='messages_per_day')],
+    [
+        path('messages_per_day', views.messages_per_day, name='messages_per_day'),
+        path('top_images', views.top_images, name='top_images'),
+    ],
     'api',
     'api',
 )
