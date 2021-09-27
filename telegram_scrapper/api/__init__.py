cfbcf480f254e82e130f314a56d9a910e3753d83
Luiz Cavalcanti <cavalcanti.luiz@gmail.com>
2021-09-27 17:41:51 -0300

    Tabela com top 5 usuários que mais postam

--- telegram_scrapper/api/__init__.py
+++ telegram_scrapper/api/__init__.py
@@ -6,6 +6,7 @@ urls = (
     [
         path('messages_per_day', views.messages_per_day, name='messages_per_day'),
         path('top_images', views.top_images, name='top_images'),
+        path('top_users', views.top_users, name='top_users'),
     ],
     'api',
     'api',
