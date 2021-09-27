7961a1721d975b025a6f6ea7d9eeec8de9e48031
Luiz Carlos Cavalcanti <luiz.c@thoughtworks.com>
2021-09-27 17:41:51 -0300

    Tabela com top 5 usuários que mais postam

--- telegram_scrapper/api/views.py
+++ telegram_scrapper/api/views.py
@@ -3,8 +3,9 @@ from datetime import datetime, timedelta
 from django.db.models import Count
 from django.db.models.functions import TruncDate
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
 
-from telegram_scrapper.core.models import Message
+from telegram_scrapper.core.models import Message, TelegramUser
 
 
 _DEFAULT_DAYS = 30
@@ -42,3 +43,30 @@ def top_images(request):
     )
 
     return JsonResponse(list(data), safe=False)
+
+
+def top_users(request):
+    past_days = int(request.GET.get('days', _DEFAULT_DAYS))
+    limit = int(request.GET.get('limit', _DEFAULT_LIMIT))
+    start_date = datetime.today() - timedelta(days=past_days)
+    end_date = datetime.today() + timedelta(days=1)
+    data = (
+        Message.objects.filter(sent_at__range=[start_date.date(), end_date.date()])
+        .exclude(sender='channel')
+        .annotate(count=Count('sender'))
+        .order_by('-count')
+        .values('sender')
+        .annotate(**{'total': Count('sender')})[:limit]
+    )
+
+    # Que presepada, preciso botar uma FK nesse negócio logo
+    result_data = list(data)
+    for entry in result_data:
+        try:
+            entry['user'] = model_to_dict(
+                TelegramUser.objects.get(user_id=entry['sender'])
+            )
+        except Exception as e:
+            pass
+
+    return JsonResponse(result_data, safe=False)
