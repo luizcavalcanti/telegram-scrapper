8577d7434ab4aee145f96061a7d8647e5adb5c7c
Luiz Carlos Cavalcanti <luiz.c@thoughtworks.com>
2022-01-09 16:20:37 -0300

    New dashboard initial templates

---
+++ telegram_scrapper/dashboard/__init__.py
@@ -0,0 +1,55 @@
+import json
+
+from django.contrib.postgres.search import SearchQuery
+from django.core.paginator import Paginator
+from django.db.models import Count, F
+from django.db.models.functions import TruncDate
+from django.shortcuts import render
+from django.urls import path
+
+from telegram_scrapper.core.models import Group, Message, TelegramUser
+
+
+def home(request):
+    context_data = {
+        'total_messages': Message.objects.count(),
+        'total_groups': Group.objects.count(),
+        'total_users': TelegramUser.objects.count()
+    }    
+    return render(request, 'dashboard.html', context_data)
+
+
+def _occurency_for_messages(queryset):
+    return queryset.annotate(date=TruncDate('sent_at')).order_by('date').values('date').annotate(**{'total': Count('sent_at')})
+
+
+def message_search(request):
+    query = request.GET.get('query')
+    page_number = request.GET.get('page', 1)
+    search_query = SearchQuery(query)
+    results = Message.objects.filter(search_vector=search_query).order_by("-sent_at")
+    occurency = _occurency_for_messages(results)
+
+    paginator = Paginator(results, 20)
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'message_search.html',
+        {
+            'query': query,
+            'results': page_obj,
+            'results_count': paginator.count,
+            'occurency': occurency
+        }
+    )
+
+
+urls = (
+    [
+        path('', home, name='dashboard'),
+        path('message_search/', message_search, name='message_search')
+    ],
+    '',
+    '',
+)
